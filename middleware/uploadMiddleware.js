09b42d4b88e6d99f6e138c868ffec6c9a08d6eea
Student <student@example.com>
2026-08-11 19:55:36 +0700

    membuat dan mengisi uploadMiddleware.js

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    filename: (req, file, cb) => {
+        const uniqueName = Date.now() + '-' + file.originalname;
+        cb(null, uniqueName);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png/;
+
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error('Hanya file gambar yang diperbolehkan (jpeg, jpg, png).'));
+    }
+
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
+
+module.exports = upload;
