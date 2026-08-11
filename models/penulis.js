336cc0d5708fda86c4db9421263d232b16c56612
Student <student@example.com>
2026-08-11 19:55:36 +0700

    membuat dan mengisi penulis.js

---
+++ models/penulis.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const Penulis = sequelize.define("Penulis", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    tableName: "penulis",
+    timestamps: true
+  });
+
+  return Penulis;
+};
